7303ef5294109eca8f7a6dce0379223fff673dcb
Yamil Escobar <joseyamilej@hotmail.com>
2022-11-12 22:43:32 +0100

    Actions, Login de usuario

--- src/modules/auth/composables/useAuth.js
+++ src/modules/auth/composables/useAuth.js
@@ -6,14 +6,19 @@ const useAuth = () => {
     const store = useStore()
     
     const createUser = async( user ) => {
-        
         const resp = await store.dispatch('auth/createUser', user)
+        return resp
+    }
+
+    const loginUser = async( user ) => {
+        const resp = await store.dispatch('auth/signInUser', user)
         return resp
     }
 
 
     return {
-        createUser
+        createUser,
+        loginUser
     }
 }
 
--- src/modules/auth/store/actions.js
+++ src/modules/auth/store/actions.js
@@ -24,5 +24,26 @@ export const createUser = async ({ commit }, user) => {
 
 }
 
+export const signInUser = async ({ commit }, user) => {
+
+    const { email, password } = user
+
+    try {
+
+            const { data } = await authApi.post(':signInWithPassword', { email, password, returnSecureToken: true})
+            const { displayName, idToken, refreshToken } = data
+            
+            user.name = displayName
+            
+            commit('LoginUser', { user, idToken, refreshToken })
+
+            return { ok: true }
+
+    } catch (error) {
+        return { ok: false, message: error.response.data.error.message}
+    }
+
+}
+
 
 
